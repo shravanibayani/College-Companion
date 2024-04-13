012c3b2fd482d18c4490270d41181c23302d1037
shravanibayani <shravanibayani02@gmail.com>
2024-04-13 20:04:23 +0530

    finalize to-do list

--- static/script.js
+++ static/script.js
@@ -1,8 +1,8 @@
-let task = document.querySelectorAll(".task");
-let sidebar_elements_text = document.querySelectorAll(".e");
-let sidebar_elements = document.querySelectorAll(".e1");
-let special = document.querySelectorAll(".special");
-let add_button = document.querySelector(".add-button");
+// let task = document.querySelectorAll(".task");
+// let sidebar_elements_text = document.querySelectorAll(".e");
+// let sidebar_elements = document.querySelectorAll(".e1");
+// let special = document.querySelectorAll(".special");
+// let add_button = document.querySelector(".add-button");
 // sidebar_elements.forEach(element => {
 //     element.style.display = "none";
 // })
@@ -16,25 +16,44 @@ let add_button = document.querySelector(".add-button");
 // });
 
 let prev = null;
-sidebar_elements_text.forEach((element, index) => {
-    element.addEventListener("click", () => {
-        if (prev !== null) {
-            if (!sidebar_elements_text[prev].classList.contains("special")) {
-                sidebar_elements_text[prev].classList.remove("feature-active");
-            }
+// sidebar_elements_text.forEach((element, index) => {
+//     element.addEventListener("click", () => {
+//         if (prev !== null) {
+//             if (!sidebar_elements_text[prev].classList.contains("special")) {
+//                 sidebar_elements_text[prev].classList.remove("feature-active");
+//             }
+//             else {
+//                 sidebar_elements_text[prev].classList.remove("special-active");
+//             }
+//             sidebar_elements[prev].style.display = "none";
+//         }
+//         if (element.classList.contains("special")) {
+//             element.classList.add("special-active");
+//         }
+//         else {
+//             element.classList.add("feature-active");
+//         }
+//         sidebar_elements[index].style.display = "block";
+//         prev = index;
+//     });
+// });
 
-            else {
-                sidebar_elements_text[prev].classList.remove("special-active");
-            }
-            sidebar_elements[prev].style.display = "none";
-        }
-        if (element.classList.contains("special")) {
-            element.classList.add("special-active");
+function active(element) {
+    if (prev !== null) {
+        if (!sidebar_elements_text[prev].classList.contains("special")) {
+            sidebar_elements_text[prev].classList.remove("feature-active");
         }
         else {
-            element.classList.add("feature-active");
+            sidebar_elements_text[prev].classList.remove("special-active");
         }
-        sidebar_elements[index].style.display = "block";
-        prev = index;
-    });
-});
+        sidebar_elements[prev].style.display = "none";
+    }
+    if (element.classList.contains("special")) {
+        element.classList.add("special-active");
+    }
+    else {
+        element.classList.add("feature-active");
+    }
+    sidebar_elements[index].style.display = "block";
+    prev = element;
+}
